0814777ce1ebb127ee6f623d2bec10ac5604e615
Rahaf Alhabashi <rahafalhabashi@gmail.com>
2022-03-24 14:00:44 -0400

    commit tree fe1b9b519aee76df9f13e960c2f629bf2f9be744

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Add your code here
+const submitDatas = {
+    name: "Steve",
+    email: "steve@steve.com",
+}
+
+function submitData() {
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(submitDatas)
+    })
+    .then(resp => resp.json())
+    .then(data => console.log(data.id))
+    const p = body.createElement('p')
+    p.append(document.querySelector(data.id))
+}
+
+// const configurationObject = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         Accept: 'application/json',
+//     },
+//     body: JSON.stringify(submitData)
+// }
+
+// fetch('http://localhost:3000/dogs', configurationObject)
+//     .then(resp => resp.json()) //converts body of response from JSON to a plain JS object
+//     .then(object => console.log(object))
+// // second .then makes the response (converted object) available --> wahtever resp.json() returns will be logged in console.log(object)
+
+// .catch(error => {
+//     alert('Bad things have happened!')
+//     console.log(error.message)
+//     })
+
+
